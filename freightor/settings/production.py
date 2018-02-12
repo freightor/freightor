4083c5a0c79da052345812885742430d6c77a4f0
ardenn <ardenn@users.noreply.github.com>
2018-02-12 15:52:36 +0000

    Added heroku domain to allowe hosts

--- freightor/settings/production.py
+++ freightor/settings/production.py
@@ -3,7 +3,7 @@ import os
 import dj_database_url
 
 DEBUG = os.environ.get("DB_DEBUG")
-ALLOWED_HOSTS = ["herokuapp.com"]
+ALLOWED_HOSTS = [".herokuapp.com"]
 SECRET_KEY = os.environ.get('SECRET_KEY')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 DATABASES = {
